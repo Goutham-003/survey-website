a31dd9d59bd99b63a1cbd7b5a6956c0e078ae633
Goutham <sgouthamgiridhar@gmail.com>
2023-11-17 21:19:36 +0530

    Final commit

--- src/Components/ThankYouPage.js
+++ src/Components/ThankYouPage.js
@@ -7,12 +7,11 @@ return (
 	<div className="container-fluid qform"> 
 			<div className="col-md-5 m-auto"> 
 				<div className="mt-3"> 
-					<div className="card text-left h-100"> 
-						<div className="card-body my-3"> 
-							<h3>Thank You for your Response!</h3> 
+						<div className="card my-3 p-3"> 
+							<h3>Thank You for your Response!</h3>
+							<img className='tick-img' src={"/submitted.png"} alt="Submitted"/>
 							<h6>You may close this tab now.</h6> 
 						</div> 
-					</div> 
 				</div> 
 			</div> 
 		</div> 
